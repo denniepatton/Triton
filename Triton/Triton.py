fixed bug in MAD calculation

Triton/Triton.py
@@ -16,7 +16,7 @@ from triton_helpers import *
 # constants and defaults
 iupac_trans = str.maketrans('ACGTURYSWKMBDHVNX',
                             '06600336033422430') # likelihood of GC content for each bp code, times 6 (common factor)
-bin_range = np.array(range(15, 500, 5))  # hardcoded for dict-based analyses of range 15-500 > bins for Shannon Entropy
+bin_range = np.array(range(15, 505, 5))  # [15, 20, . . . , 495, 500] hardcoded for dict-based analyses of range 15-500 > bins for Shannon Entropy
 freq_max = 1.0 / 146.0  # theoretical minimum nucleosome period is 146 bp -> f = 0.0068493
 low_1, high_1 = 0.00556, 0.00667  # range_1: T = 150-180 bp (f = 0.00556 - 0.00667, "small linker")
 low_2, high_2 = 0.00476, 0.00555  # range_2: T = 180-210 bp (f = 0.00476 - 0.00555, "large linker")
@@ -84,7 +84,7 @@ def generate_profile(region, params):
         bias_limit = 0.10  # use a stricter GC-bias range for composite regions where depth is higher
     bam = pysam.AlignmentFile(bam_path, 'rb')
     ref_seq = pysam.FastaFile(ref_seq_path)
-    fragment_lengths = np.zeros(frag_range[1] + 1, dtype=int)  # empty fragment length *histogram*
+    fragment_lengths = np.zeros(frag_range[1], dtype=int)  # empty fragment length *histogram*
     skipped_sites = []  # for storing skipped sites in composite-window mode
 
     if stack:
@@ -99,8 +99,8 @@ def generate_profile(region, params):
             for entry in sites_file:  # iterate through regions in this particular BED file
                 site_depth, site_nc_signal = np.zeros(roi_length), np.zeros(roi_length)
                 site_fragment_ends = np.zeros(window)
-                site_fragment_lengths = np.zeros(frag_range[1] + 1, dtype=int)
-                site_fragment_length_profile = np.zeros((frag_range[1] + 1, window), dtype=int)
+                site_fragment_lengths = np.zeros(frag_range[1], dtype=int)
+                site_fragment_length_profile = np.zeros((frag_range[1], window), dtype=int)
                 bed_tokens = entry.strip().split('\t')
                 if not bed_tokens[pos_idx].isdigit(): continue  # header or typo line
                 chrom, center_pos = str(bed_tokens[chr_idx]), int(bed_tokens[pos_idx])
@@ -148,7 +148,7 @@ def generate_profile(region, params):
                         fragment_cov = np.arange(fragment_start, fragment_end)
                         center_in_window = 500 <= np.take(fragment_cov, abs_length // 2) <= roi_length - 501
                         if center_in_window:
-                            site_fragment_lengths[abs_length] += 1
+                            site_fragment_lengths[abs_length - 1] += 1
                         nc_density = fdict[abs_length] if 146 <= abs_length <= 500 else None
                         if bias_limit < fragment_bias < 10:
                             if strand == '+':  # positive strand:
@@ -158,7 +158,7 @@ def generate_profile(region, params):
                                     if nc_density is not None:
                                         site_nc_signal[index] += nc_density[place] / fragment_bias
                                     if 500 <= index <= roi_length - 501:
-                                        site_fragment_length_profile[abs_length, index - 500] += 1
+                                        site_fragment_length_profile[abs_length - 1, index - 500] += 1
                                         if place == 0 or place == len(fragment_cov) - 1:
                                             site_fragment_ends[index - 500] += 1
                             else:  # negative strand:
@@ -168,7 +168,7 @@ def generate_profile(region, params):
                                     if nc_density is not None:
                                         site_nc_signal[roi_length - index -1] += nc_density[place] / fragment_bias
                                     if 500 <= index <= roi_length - 501:  # no buffer for frag lengths
-                                        site_fragment_length_profile[abs_length, roi_length - index - 501] += 1
+                                        site_fragment_length_profile[abs_length - 1, roi_length - index - 501] += 1
                                         if place == 0 or place == len(fragment_cov) - 1:
                                             site_fragment_ends[roi_length - index - 501] += 1
                 # check for egregious outliers in site, and drop site if found
@@ -209,13 +209,13 @@ def generate_profile(region, params):
             center_pos = int(bed_tokens[pos_idx])
             start_pos = center_pos - int(roi_length / 2)
             stop_pos = center_pos + int(roi_length / 2)
-            fragment_length_profile = np.zeros((frag_range[1] + 1, window), dtype=int)
+            fragment_length_profile = np.zeros((frag_range[1], window), dtype=int)
             fragment_end_profile = np.zeros(window)
         else:
             start_pos = int(bed_tokens[start_idx]) - 500
             stop_pos = int(bed_tokens[stop_idx]) + 500
             roi_length = stop_pos - start_pos
-            fragment_length_profile = np.zeros((frag_range[1] + 1, roi_length - 1000), dtype=int)
+            fragment_length_profile = np.zeros((frag_range[1], roi_length - 1000), dtype=int)
             fragment_end_profile = np.zeros(roi_length - 1000)
         depth, nc_signal = np.zeros(roi_length), np.zeros(roi_length)
         # process all fragments falling inside the site
@@ -245,7 +245,7 @@ def generate_profile(region, params):
                 fragment_cov = np.arange(fragment_start, fragment_end)
                 center_in_window = 500 <= np.take(fragment_cov, abs_length // 2) <= roi_length - 501
                 if center_in_window:
-                    fragment_lengths[abs_length] += 1
+                    fragment_lengths[abs_length - 1] += 1
                 nc_density = fdict[abs_length] if 146 <= abs_length <= 500 else None
                 if bias_limit < fragment_bias < 10:
                     for place, index in enumerate(fragment_cov):
@@ -255,7 +255,7 @@ def generate_profile(region, params):
                         if nc_density is not None:
                             nc_signal[index] += nc_density[place] / fragment_bias
                         if 500 <= index <= roi_length - 501:
-                            fragment_length_profile[abs_length, index - 500] += 1
+                            fragment_length_profile[abs_length - 1, index - 500] += 1
                             if place == 0 or place == len(fragment_cov) - 1:
                                 fragment_end_profile[index - 500] += 1
         if strand == "-":
@@ -314,10 +314,10 @@ def generate_profile(region, params):
     if window is not None and len(peaks) > 0 and len(troughs) > 0:
         # first find the peak/trough nearest to the window's center
         inflection_loc = np.argmin(np.abs(np.concatenate([peaks, troughs]) - int(window / 2))) + int(window/2)
-        inflection = phased_signal[inflection_loc] # value at the inflection location
         if not np.isnan(inflection_loc):
             peak_profile[inflection_loc] = 3
             center = len(phased_signal) // 2
+            inflection = phased_signal[inflection_loc] # value at the inflection location
             inflection_depth = inflection / np.mean(np.concatenate((phased_signal[:center-250], phased_signal[center+250:]))) # inflection depth relative to the mean of the flanking regions
             left_max_loc, right_max_loc = nearest_peaks(inflection_loc, peaks)
             if not np.isnan(left_max_loc) and not np.isnan(right_max_loc):

Triton/triton_helpers.py
@@ -63,20 +63,26 @@ def get_gc_bias_dict(bias_path):
 
 def frag_metrics(frag_lengths, bins, reduce=False):
     """
-    Returns the mean, standard deviation, median, median absolute deviation, ratio of short (f <= 150)
-    to long (151 <= f)*, diversity, and Shannon entropy of fragment lengths given an input of fragment length counts.
-    * Used to be short (f <= 120) to long (140 <= f <= 250)
-        Parameters:
-            frag_lengths (np.array): 1D array of fragment length counts, where the index is the fragment length
-            bins (np.array): list of bin boundaries
-            reduce (bool): if true, only output ratio, diversity, and entropy (save time on signal profiles)
-        Returns:
-            mean, stdev, MAD, ratio (float, float, int, float)
-    N.B. since a histogram-array is being passed, I am doing some "fancy math" to save time and space
+    Returns various metrics of fragment lengths given an input of fragment length counts.
+    
+    Parameters:
+        frag_lengths (np.array): 1D array of fragment length counts, where the index is the fragment length - 1 (1 indexed)
+        bins (np.array): list of bin boundaries
+        reduce (bool): if true, only output ratio, diversity, and entropy (save time on signal profiles)
+    
+    Returns:
+        Tuple containing:
+            - mean (float)
+            - stdev (float)
+            - median (float)
+            - mad (float)
+            - ratio (float)
+            - diversity (float)
+            - entropy (float)
     """
     total_count = np.sum(frag_lengths)
     unique_count = np.count_nonzero(frag_lengths)
-    frag_lengths_long = np.sum(frag_lengths[151:])
+    frag_lengths_long = np.sum(frag_lengths[150:])
     
     if total_count < 2 or unique_count < 2 or frag_lengths_long == 0:  # BARE MINIMUM to get real metrics
         return (0.0,) * (3 if reduce else 7)
@@ -88,35 +94,43 @@ def frag_metrics(frag_lengths, bins, reduce=False):
     if reduce:
         return ratio, diversity, entropy
 
-    mean = np.average(np.arange(len(frag_lengths)), weights=frag_lengths)
-    stdev = np.sqrt(np.average((np.arange(len(frag_lengths)) - mean)**2, weights=frag_lengths))
-    median = np.searchsorted(np.cumsum(frag_lengths), total_count / 2)
-    mad = np.searchsorted(np.cumsum(np.bincount(np.abs(np.arange(len(frag_lengths)) - median), minlength=len(frag_lengths))), total_count / 2)
+    # Calculate mean and standard deviation
+    indices = np.arange(len(frag_lengths)) + 1
+    mean = np.average(indices, weights=frag_lengths)
+    stdev = np.sqrt(np.average((indices - mean) ** 2, weights=frag_lengths))
+
+    # Calculate median
+    cumulative_counts = np.cumsum(frag_lengths)
+    median = np.searchsorted(cumulative_counts, total_count / 2) + 1
+
+    # Calculate MAD (Median Absolute Deviation)
+    deviations = np.abs(indices - median)
+    sorted_indices = np.argsort(deviations)
+    sorted_deviations = deviations[sorted_indices]
+    sorted_counts = frag_lengths[sorted_indices]
+    cumulative_counts = np.cumsum(sorted_counts)
+    median_index = np.searchsorted(cumulative_counts, total_count / 2)
+    mad = sorted_deviations[median_index]
 
     return mean, stdev, median, mad, ratio, diversity, entropy
 
 
 def shannon_entropy(counts, bins):
     """
-    Returns the Shannon Entropy of a set of values; will bin with "bins" bins
+    Returns the max-normalized Shannon Entropy of a set of values, ignoring bins with 0 counts.
         Parameters:
-            counts (list/array): 1D array of fragment length counts, where the index is the fragment length
-            bins (np.array): list of bin boundaries
+            counts (list/array): 1D array of fragment length counts, where the index is the fragment length -1 (1 indexed).
+            bins (np.array): list of bin boundaries.
         Returns:
-            float: Shannon Entropy
+            float: Max-normalized Shannon Entropy.
     """
-    # Calculate histogram using numpy's advanced indexing and binning
-    histogram = np.add.reduceat(counts, bins[:-1])
-    total_counts = histogram.sum()
-
-    if total_counts < 2:
-        return 0
-
-    pdist = histogram / total_counts
-    nonzero_pdist = pdist[pdist != 0.0]
-    moments = nonzero_pdist * np.log(nonzero_pdist) / np.log(total_counts)
-
-    return -moments.sum()
+    histogram, _ = np.histogram(counts, bins=bins)
+    non_zero_histogram = histogram[histogram > 0]
+    total_counts = non_zero_histogram.sum()
+    pdist = non_zero_histogram / total_counts
+    entropy = -np.sum(pdist * np.log2(pdist))
+    max_entropy = np.log2(len(bins))
+    return entropy / max_entropy if len(non_zero_histogram) > 1 else 0.0
 
 
 def local_peaks(ys, xs):